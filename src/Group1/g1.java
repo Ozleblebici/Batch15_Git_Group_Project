c5c32dccd18fdac0b4823ba46a7920fb94084163
Talha <talha43gs@gmail.com>
2024-03-26 21:17:49 +0000

    base project

---
+++ src/Group1/g1.java
@@ -0,0 +1,9 @@
+package Group1;
+
+public class g1 {
+    public static void main(String[] args) {
+        System.out.println("test111");
+        System.out.println("test222");
+        System.out.println("test333");
+    }
+}
---
+++ src/Group3/g3.java
@@ -0,0 +1,9 @@
+package Group3;
+
+public class g3 {
+    public static void main(String[] args) {
+        System.out.println("test111");
+        System.out.println("test222");
+        System.out.println("test333");
+    }
+}
