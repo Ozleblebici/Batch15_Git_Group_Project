3840a55c7c2bea5c198f8b99e8adbc57b468c354
Talha <talha43gs@gmail.com>
2024-03-27 17:29:29 +0000

    payment test uploaded

---
+++ src/PaymentTest.java
@@ -0,0 +1,8 @@
+public class PaymentTest {
+
+    public static void main(String[] args) {
+        System.out.println("Payment test1");
+        System.out.println("Payment test3");
+        System.out.println("Payment test4");
+    }
+}
