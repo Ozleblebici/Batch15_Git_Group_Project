fab2a91150dfa9665d274f70fac717b60810305d
Sarita <patelsarita@gmail.com>
2024-03-27 14:11:13 +0100

    I have updated my Test2

---
+++ src/Group2/Test2.java
@@ -0,0 +1,10 @@
+package Group2;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test Homework");
+        System.out.println("Test Homework1");
+        System.out.println("Test Homework2");
+        System.out.println("Test Homework3");
+    }
+}
