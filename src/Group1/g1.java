c69aa52befce43db493914dcfb39e4edc361ad0e
HasnaTrk <hasnacelebi@hotmail.com>
2024-03-27 15:14:04 +0300

    Hasna updated

--- src/Group1/g1.java
+++ src/Group1/g1.java
@@ -5,5 +5,6 @@ public class g1 {
         System.out.println("test111");
         System.out.println("test222");
         System.out.println("test333");
+        System.out.println("I am adding something");
     }
 }
