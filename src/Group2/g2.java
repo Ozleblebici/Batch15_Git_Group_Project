96a224eaff197261fc95ee1aeb25c8c307071d8f
Rosana <rosanakocova@yahoo.com>
2024-03-27 17:31:17 +0100

    Tests are ready from Branch named Rosana1

--- src/Group2/g2.java
+++ src/Group2/g2.java
@@ -2,7 +2,7 @@ package Group2;public class g2 {
     public static void main(String[] args) {
       System.out.println("test111");
       System.out.println("This is Group 2 and Sarita wrote this code;)");
-      System.out.println("test Rosana");
+      System.out.println("test Rosana1234");
       System.out.println("test333");
     }
 }
