edfa9e4537f64c491528d525a382823cd595475a
Rosana <rosanakocova@yahoo.com>
2024-03-27 12:18:24 +0100

    Tests are ready from Branch named Rosana

---
+++ src/Group2/Test1.java
@@ -0,0 +1,10 @@
+package Group2;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Test1233");
+        System.out.println("Test12");
+        System.out.println("Test14533");
+        System.out.println("Test1257933");
+    }
+}
--- src/Group2/g2.java
+++ src/Group2/g2.java
@@ -1,7 +1,7 @@
 package Group2;public class g2 {
     public static void main(String[] args) {
         System.out.println("test111");
-        System.out.println("test222");
+        System.out.println("test Rosana");
         System.out.println("test333");
     }
 }
