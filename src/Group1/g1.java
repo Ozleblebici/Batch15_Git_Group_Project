72fc1269c6e770126b0dc93aeac6843ec57b0657
HasnaTrk <hasnacelebi@hotmail.com>
2024-03-27 16:47:02 +0300

    Hasna updated

--- src/Group1/g1.java
+++ src/Group1/g1.java
@@ -2,7 +2,7 @@ package Group1;
 
 public class g1 {
     public static void main(String[] args) {
-        System.out.println("test105");
+        System.out.println("Test105");
         System.out.println("test222");
         System.out.println("test333");
     }
