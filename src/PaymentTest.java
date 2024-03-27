cf9fcf3dc7f5d8cd090e1a4063e60f203594ca24
Talha Ozleblebici <talha43gs@gmail.com>
2024-03-27 17:48:47 +0000

    Revert "TS 1009 Payment Module Automation Test "

--- src/PaymentTest.java
+++
@@ -1,8 +0,0 @@
-public class PaymentTest {
-
-    public static void main(String[] args) {
-        System.out.println("Payment test1");
-        System.out.println("Payment test3");
-        System.out.println("Payment test4");
-    }
-}
